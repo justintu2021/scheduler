c8ee0521da45767f58cd2a6b7059d4464dc7324b
Tran Lam Tu <justin.tu@Trans-MacBook-Air.local>
2021-12-09 14:24:53 -0500

    create button and DayList and Daylistitem components

---
+++ src/components/DayList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+const DayList = (props) => {
+
+  const listOfDay = props.days.map((element) => {
+      return <DayListItem key={element.id}
+        name={element.name} 
+        spots={element.spots} 
+        selected={element.name === props.day}
+        setDay={props.setDay} />
+  })
+  return(
+    <section className="Day List">
+        <h1>Day List</h1>
+        <ul>{listOfDay}</ul>
+      </section>
+  )
+}
+
+export default DayList;
